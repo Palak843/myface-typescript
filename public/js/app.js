f8b1943c795a49396c5d9c511db8a36fa57a2c8b
Palak Mittal <Palak.Mittal@softwire.com>
2022-05-12 13:51:49 +0100

    webpage

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,16 @@
+function changeColor() {
+    const posts = document.getElementsByClassName("post");
+
+    for (let i=0; i<posts.length; i++){
+        const post = posts[i];
+        const red = Math.floor(Math.random()*256);
+        const green = Math.floor(Math.random()*256);
+        const blue =Math.floor(Math.random()*256);
+
+        post.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+
+    }
+}
 document.addEventListener('DOMContentLoaded', function() {
     // Add your JavaScript here.
 });
